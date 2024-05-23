b716992f68a6e87191d375a9be99a52a7a846acf
haithanhkc42 <haidthe170573@fpt.edu.vn>
2024-05-24 01:53:42 +0700

    add profile, change password

--- src/java/Controller/ChangePasswordControl.java
+++ src/java/Controller/ChangePasswordControl.java
@@ -18,58 +18,17 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  * @author lvhn1
  */
-@WebServlet(name = "ChangePasswordControl", urlPatterns = {"/change-password"})
+@WebServlet(name = "ChangePasswordControl", urlPatterns = {"/common/change-pass"})
 public class ChangePasswordControl extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ChangePasswordControl</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ChangePasswordControl at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        request.getRequestDispatcher("../change-pass.jsp").forward(request, response);
+        
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -86,21 +45,12 @@ public class ChangePasswordControl extends HttpServlet {
             user.setPassword(password);
             new UserDAO().updateUser(user);
             
-            response.sendRedirect("userprofile?successcp");
+            response.sendRedirect("change-pass?success");
 
-        } else response.sendRedirect("userprofile?failcp");
+        } else response.sendRedirect("change-pass?fail");
 
         
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
