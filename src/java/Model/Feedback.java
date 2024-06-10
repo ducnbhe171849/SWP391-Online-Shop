0e811caa2af33f8236242a5cece3d7acfd13b749
tuanphung2003 <tuanpahs171369@fpt.edu.vn>
2024-06-10 14:55:02 +0700

    fix bug feedback list and feedback details

--- src/java/Model/Feedback.java
+++ src/java/Model/Feedback.java
@@ -1,6 +1,8 @@
 package Model;
 
+import DAO.OrderDAO;
 import java.util.Date;
+import javax.xml.bind.DatatypeConverter;
 
 public class Feedback {
     private int id;
@@ -10,7 +12,83 @@ public class Feedback {
     private boolean isDeleted;
     private Date createdAt;
     private int createdBy;
+    private byte[] imgeURL;
 
+    public byte[] getImgeURL() {
+        return imgeURL;
+    }
+
+    public void setImgeURL(byte[] imgeURL) {
+        this.imgeURL = imgeURL;
+    }
     
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrderDetailId() {
+        return orderDetailId;
+    }
+
+    public void setOrderDetailId(int orderDetailId) {
+        this.orderDetailId = orderDetailId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public boolean isIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public OrderDetail getOrderDetail() {
+        return new OrderDAO().getOrderDetailById(orderDetailId);
+    }
+    
+    public String getImage() {
+        if (imgeURL == null) return "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg";
+        
+        String imageBase64 = DatatypeConverter.printBase64Binary(imgeURL);
+        return "data:image/png;base64," + imageBase64;
+    }
 
 }
